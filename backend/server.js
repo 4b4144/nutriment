1c6d2c7cdd81ca6577bbe4ed517297a0c79632b3
4b4144 <abdelkader.arkoun.auditeur@lecnam.net>
2023-10-19 17:31:31 +0200

    Base du back

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const routesHandler = require('./routes.js');
+
+const app = express();
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.json());
+
+app.use('/', routesHandler);
+
+const port = 3000; // port du backend
+
+app.listen(port, () => {
+console.log('Serveur démarré au port ' + port);
+});
