5f84c989c51518777bfd1661a1246ece2c463277
4b4144 <abdelkader.arkoun.auditeur@lecnam.net>
2023-10-20 21:13:24 +0200

    Back amelioré

--- backend/routes.js
+++ backend/routes.js
@@ -5,28 +5,63 @@ const SparqlClient = require('sparql-http-client')
 const endpointUrl = 'http://localhost:3030/aliment/sparql';
 
 
-router.get('/aliments', async (req, res) => {
+router.get('/', async (req, res) => {
     try {
-    const query = 
-    `
-    SELECT ?value
-    WHERE {
-      ?s ?p ?value
-    }
-  `;
-  const client = new SparqlClient({endpointUrl});
-  const stream = await client.query.select(query);
-  stream.on('data', row => {
-    Object.entries(row).forEach(([key, value]) => {
-      console.log(`${key}: ${value.value} (${value.termType})`)
-    })
-  });
+        const query =
+        `
+        SELECT DISTINCT ?value
+        WHERE {
+        ?s ?p ?value
+        }
+    `;
+        const client = new SparqlClient({ endpointUrl });
+        const stream = await client.query.select(query);
+        stream.on('data', row => {
+            Object.entries(row).forEach(([key, value]) => {
 
-  res.status(200).json({message : 'Succès'});
+            });
+        });
+
+        res.status(200).json({ message: 'Succès' });
     } catch (error) {
         console.log('Erreur lors de la récupération des données :', error);
         res.status(500).json({ error: 'Erreur lors de la récupération des donnée.' });
     }
+});
+
+
+router.get('/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const query = `
+        SELECT DISTINCT ?predicate ?object
+        WHERE {
+          <http://127.0.0.1:3333/id=${id}> ?predicate ?object .
+        }
+      `;
+        const client = new SparqlClient({ endpointUrl });
+        const stream = await client.query.select(query);
+
+        const result = [];
+
+        stream.on('data', row => {
+            dataObject = {};
+            Object.entries(row).forEach(([key, value]) => {
+                dataObject[key] = value.value;
+            });
+            result.push(dataObject);
+        });
+
+        stream.on('end', () => {
+            res.status(200).json(result);
+        });
+
+    } catch (error) {
+        console.log('Erreur lors de la récupération des données :', error);
+        res.status(500).json({ error: 'Erreur lors de la récupération des donnée.' });
+    }
+
+
 });
 
 
--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/', routesHandler);
+app.use('/aliments', routesHandler);
 
 const port = 3000; // port du backend
 
