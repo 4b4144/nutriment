4ab9eb0e3a7b75a262a8e442a79f96ee7f93969e
4b4144 <abdelkader.arkoun.auditeur@lecnam.net>
2023-11-04 17:32:38 +0100

    Update h-full

--- frontend/src/components/details-aliment/details-aliment.js
+++ frontend/src/components/details-aliment/details-aliment.js
@@ -4,7 +4,7 @@ const DetailsAliment = () =>{
     return(
         <>
             <div className="flex items-center justify-center w-full mt-4 mb-4 p-4 h-full border-2">
-               <div className="w-4/5 h-screen  border-2">
+               <div className="w-4/5 h-full  border-2">
                 Détails Aliments
                </div>
             </div>
--- frontend/src/components/search-aliment/search-aliment.js
+++ frontend/src/components/search-aliment/search-aliment.js
@@ -4,7 +4,7 @@ const SearchAliment = () => {
     return (
         <>
             <div className="flex items-center justify-center w-full mt-4 mb-4 p-4 h-full">
-                <div className="w-4/5 h-screen">
+                <div className="w-4/5 h-full">
                     Rechercher un aliment
                 </div>
             </div>
