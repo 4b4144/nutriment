b4f25e76e8174c43172be627972d0e2a31a5b393
4b4144 <abdelkader.arkoun.auditeur@lecnam.net>
2023-11-08 08:48:04 +0100

    Route de recherche fonctionnelle

--- backend/routes.js
+++ backend/routes.js
@@ -4,6 +4,49 @@ const SparqlClient = require('sparql-http-client')
 
 const endpointUrl = 'http://localhost:3030/aliment/sparql';
 
+
+
+// route pour http:localhost:4000/aliments/search?recherche="...."
+router.get('/search', async (req, res) => {
+    try {
+        const stringQuery = req.query.recherche;
+        console.log(req.query);
+        if (stringQuery) {
+            const query =
+                `
+                SELECT DISTINCT (strafter(str(?id), "=") AS ?aliment_id) ?nom ?nomgrp ?image
+                WHERE {
+                  ?id a ?type .
+                  ?id <http://127.0.0.1:3333/alim/alim_nom_fr> ?nom .
+                  ?id <http://127.0.0.1:3333/alim/alim_grp_nom_fr> ?nomgrp.
+                  OPTIONAL { ?id <http://127.0.0.1:3333/alim/alim_img> ?image }
+                  FILTER (!REGEX(?nomgrp, "^[0-9]"))
+                  FILTER (STRSTARTS(UCASE(?nom), UCASE("${stringQuery}")))
+                }
+        `;
+            const client = new SparqlClient({ endpointUrl });
+            const stream = await client.query.select(query);
+            const result = [];
+
+            stream.on('data', row => {
+                dataObject = {};
+                Object.entries(row).forEach(([key, value]) => {
+                    dataObject[key] = value.value;
+                });
+                result.push(dataObject);
+            });
+
+            stream.on('end', () => {
+                res.status(200).json(result);
+            });
+        }
+
+    } catch (error) {
+        console.log('Erreur lors de la récupération des données :', error);
+        res.status(500).json({ error: 'Erreur lors de la récupération des donnée.' });
+    }
+});
+
 // route pour : http:localhost:4000/aliments
 router.get('/', async (req, res) => {
     try {
@@ -99,45 +142,6 @@ router.get('/:id', async (req, res) => {
     }
 });
 
- // route pour http:localhost:4000/aliments/search?recherche="...."
-router.get('/search', async (req, res) => {
-    try {
-        const stringQuery = req.query.recherche;
-        console.log(req.query);
-        if (stringQuery) {
-            const query =
-                `
-                SELECT DISTINCT (strafter(str(?id), "=") AS ?aliment_id) ?nom ?nomgrp ?image
-                WHERE {
-                  ?id a ?type .
-                  ?id <http://127.0.0.1:3333/alim/alim_nom_fr> ?nom .
-                  ?id <http://127.0.0.1:3333/alim/alim_grp_nom_fr> ?nomgrp.
-                  OPTIONAL { ?id <http://127.0.0.1:3333/alim/alim_img> ?image }
-                  FILTER (!REGEX(?nomgrp, "^[0-9]"))
-                  FILTER (STRSTARTS(UCASE(?nom), UCASE("${stringQuery}")))
-                }
-        `;
-            const client = new SparqlClient({ endpointUrl });
-            const stream = await client.query.select(query);
-            const result = [];
-
-            stream.on('data', row => {
-                dataObject = {};
-                Object.entries(row).forEach(([key, value]) => {
-                    dataObject[key] = value.value;
-                });
-                result.push(dataObject);
-            });
-
-            stream.on('end', () => {
-                res.status(200).json(result);
-            });
-        }
-
-    } catch (error) {
-        console.log('Erreur lors de la récupération des données :', error);
-        res.status(500).json({ error: 'Erreur lors de la récupération des donnée.' });
-    }
-});
+ 
 
 module.exports = router;
